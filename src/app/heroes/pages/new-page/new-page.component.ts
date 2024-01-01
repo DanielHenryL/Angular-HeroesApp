c70f8dd28beba6abd635c022a4b2c3c10e98d0f9
DanielHenryL <lagunasdaniel001@gmail.com>
2024-01-01 13:25:51 -0500

    Conectar el formulario con el template

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -9,8 +9,25 @@ import { Publisher } from '../../interfaces/hero.interface';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup({
+    slug: new FormControl<string>('', {nonNullable:true}),
+    superhero: new FormControl<string>('', {nonNullable:true}),
+    publisher: new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego: new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alt_img: new FormControl<string>(''),
+  });
+
   public publishers = [
     { id:'DC Comics' , desc:'DC - Comics'},
     { id:'Marvel Comics' , desc:'Marvel - Comics'},
   ]
+
+  onSubmit():void{
+    console.log( {
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    } );
+  }
 }
